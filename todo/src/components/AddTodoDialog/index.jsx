bcf8fc1078738a69b82aea7c05493eb68b3ea714
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-07-26 21:15:09 -0300

    feature: AddTodoDialog

---
+++ todo/src/components/AddTodoDialog/index.jsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import Button from "../Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Box } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { AddTodoAction } from "../../global/actions/TodoActions";
+
+export default function AddTodoDialog({ open, setOpen }) {
+    const dispatch = useDispatch();
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+                component: "form",
+                onSubmit: (event) => {
+                    event.preventDefault();
+                    const formData = new FormData(event.currentTarget);
+                    const formJson = Object.fromEntries(formData.entries());
+                    const todo = {
+                        ...formJson,
+                        created_at: JSON.stringify(new Date()),
+                    };
+                    dispatch(AddTodoAction(todo));
+
+                    console.log(formJson);
+                },
+            }}
+            fullWidth
+        >
+            <DialogTitle>Insira os dados da tarefa</DialogTitle>
+            <DialogContent>
+                <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    gap={"10px"}
+                    marginTop={"5px"}
+                >
+                    <TextField
+                        autoFocus
+                        required
+                        id="title"
+                        name="title"
+                        label="Título"
+                        variant="outlined"
+                        fullWidth
+                    />
+                    <TextField
+                        id="description"
+                        name="description"
+                        label="Descrição"
+                        fullWidth
+                        variant="outlined"
+                        multiline
+                    />
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button type="submit">Criar</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
